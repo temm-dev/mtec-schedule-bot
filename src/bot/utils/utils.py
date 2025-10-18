526d7728164fa3e79d1b88c84c93aae289f8da1a
temm-dev <temm-dev@proton.me>
2025-10-18 22:59:29 +0300

    Added a function to convert full name to initials.

--- src/bot/utils/utils.py
+++ src/bot/utils/utils.py
@@ -8,3 +8,16 @@ def day_week_by_date(date: str):
     day_of_week_number = date_object.weekday()
     day_of_week_name = days_of_week[day_of_week_number]
     return day_of_week_name
+
+def format_names(names: list[str]):
+    result = []
+    for name in names:
+        parts = name.split()
+        if len(parts) >= 3:
+            surname, first_name, patronymic = parts[0], parts[1], parts[2]
+            formatted_name = f"{surname} {first_name[0]}. {patronymic[0]}."
+            result.append(formatted_name)
+        else:
+            result.append(name)
+    
+    return result
